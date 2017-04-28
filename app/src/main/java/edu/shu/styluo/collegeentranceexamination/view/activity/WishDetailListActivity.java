78cf8b1f7f6c9815a10427a098e90c8e63fd6115
Jiahui Li <shu_jiahuili@foxmail.com>
2017-04-28 09:29:06 +0800

    加入志愿页点击事件跳转以及对应跳转页，bug：fragment中btn的selector state_hover无响应（未修复）

---
+++ app/src/main/java/edu/shu/styluo/collegeentranceexamination/view/activity/WishDetailListActivity.java
@@ -0,0 +1,20 @@
+package edu.shu.styluo.collegeentranceexamination.view.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * 排名信息列表查看
+ * author: styluo
+ * date: 2017/4/28 9:23
+ * e-mail: shu_jiahuili@foxmail.com
+ */
+
+public class WishDetailListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
--- app/src/main/java/edu/shu/styluo/collegeentranceexamination/view/fragment/WishFragment.java
+++ app/src/main/java/edu/shu/styluo/collegeentranceexamination/view/fragment/WishFragment.java
@@ -1,21 +1,34 @@
 package edu.shu.styluo.collegeentranceexamination.view.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import edu.shu.styluo.collegeentranceexamination.R;
+import edu.shu.styluo.collegeentranceexamination.view.activity.WishDetailListActivity;
 
 /**
  * 志愿页的View视图
  *@Author Created by 29043 on 2017/4/18.
  *@Time 2017.4.18
  */
-public class WishFragment extends Fragment{
+public class WishFragment extends Fragment implements View.OnClickListener{
+
+    @BindView(R.id.btn_wish_xyh)
+    Button mButtonXyh;
+    @BindView(R.id.btn_wish_wsl)
+    Button mButtonWsl;
+    @BindView(R.id.btn_wish_jd)
+    Button mButtonJd;
+    @BindView(R.id.btn_wish_qs)
+    Button mButtonQs;
 
     @Nullable
     @Override
@@ -23,6 +36,46 @@ public class WishFragment extends Fragment{
         View view = inflater.inflate(R.layout.fragment_wish, container, false);
         ButterKnife.bind(this, view);
 
+        init();
+
         return view;
+    }
+
+    /**
+     * 根据点击传递不同信息给Activity，跳转后根据info信息请求相应地址获取信息
+     * @param v
+     */
+    @Override
+    public void onClick(View v) {
+        Intent intent;
+        switch (v.getId()){
+            case R.id.btn_wish_xyh:
+                intent = new Intent(getActivity(), WishDetailListActivity.class);
+                intent.putExtra("info", "xyh");
+                startActivity(intent);
+                break;
+            case R.id.btn_wish_wsl:
+                intent = new Intent(getActivity(), WishDetailListActivity.class);
+                intent.putExtra("info", "wsl");
+                startActivity(intent);
+                break;
+            case R.id.btn_wish_jd:
+                intent = new Intent(getActivity(), WishDetailListActivity.class);
+                intent.putExtra("info", "jd");
+                startActivity(intent);
+                break;
+            case R.id.btn_wish_qs:
+                intent = new Intent(getActivity(), WishDetailListActivity.class);
+                intent.putExtra("info", "qs");
+                startActivity(intent);
+                break;
+        }
+    }
+
+    private void init(){
+        mButtonWsl.setOnClickListener(this);
+        mButtonXyh.setOnClickListener(this);
+        mButtonJd.setOnClickListener(this);
+        mButtonQs.setOnClickListener(this);
     }
 }
